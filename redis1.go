37b0018d8b5062accf87b4b59cba6d989beeab20
Erik Bergsma <ebergsma1@gmail.com>
2020-03-22 20:23:54 +0100

    small test to connect to a redis instance

---
+++ redis1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+import "github.com/go-redis/redis"
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+}
