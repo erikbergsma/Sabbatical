6624d88d1e038ae209bb2e92b51f26e814fa72fe
Erik Bergsma <ebergsma1@gmail.com>
2020-03-23 11:57:35 +0100

    get set

--- src/golang/redis1.go
+++ src/golang/redis1.go
@@ -9,7 +9,24 @@ func main() {
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
+	
+	err := client.Set("key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
 
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
+	val, err := client.Get("key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := client.Get("key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
 }
