8fcc9f3f52cd7503f5c05eae5713422283c4a427
Erik Bergsma <ebergsma1@gmail.com>
2020-03-24 12:37:26 +0100

    now supports adding and fetching of multiple objects. objects are stored as hashes in key customers:foo and an index of the objects as a set in key customers

--- src/golang/main.go
+++ src/golang/main.go
@@ -29,20 +29,44 @@ type Server struct {
 }
 
 func main() {
-	server := &Server{
+	server1 := &Server{
 		Name:    "gopher",
 		ID:      123456,
 		Enabled: true,
 	}
 
+	server2 := &Server{
+		Name:    "blooper",
+		ID:      78910,
+		Enabled: false,
+	}
+
 	// Convert a struct to a map[string]interface{}
 	// => {"Name":"gopher", "ID":123456, "Enabled":true}
-	m := structs.Map(server)
+	m := structs.Map(server1)
 	fmt.Println("map", m)
+
+	o := structs.Map(server2)
+	fmt.Println("map", o)
 
 	err := client.HSet("customer:2", m).Err()
 	if err != nil {
 		panic(err)
+	}
+
+	err2 := client.SAdd("customers", "customer:2").Err()
+	if err2 != nil {
+		panic(err2)
+	}
+
+	err3 := client.HSet("customer:3", o).Err()
+	if err3 != nil {
+		panic(err3)
+	}
+
+	err4 := client.SAdd("customers", "customer:3").Err()
+	if err4 != nil {
+		panic(err4)
 	}
 
 	// route
@@ -58,41 +82,57 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusBadRequest)
 	}
 
-	val2, err := client.HGetAll("customer:2" ).Result()
+	allcustomers, err := client.SMembers("customers").Result()
 	if err == redis.Nil {
 		fmt.Println("key2 does not exist")
 	} else if err != nil {
 		panic(err)
 	} else {
-		fmt.Println("key2", val2)
+		fmt.Println("members", allcustomers)
 	}
 
-	Enabled, err := strconv.ParseBool(val2["Enabled"])
+	//customers := make([]struct{})
+	var customers []Server
+	for _, customer := range allcustomers {
+		val2, err := client.HGetAll(customer).Result()
+		if err == redis.Nil {
+			fmt.Println("key2 does not exist")
+		} else if err != nil {
+			panic(err)
+		} else {
+			fmt.Println("key2", val2)
+		}
+
+		Enabled, err := strconv.ParseBool(val2["Enabled"])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+	    return
+		}
+
+		ID, err := strconv.Atoi(val2["ID"])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+	    return
+		}
+
+		server := Server{
+			Name:    val2["Name"],
+			ID:      ID,
+			Enabled: Enabled,
+		}
+
+		customers = append(customers, server)
+	}
+
+	w.Header().Set("Server", "A Go Web Server")
+
+	js, err := json.Marshal(customers)
 	if err != nil {
-		panic(err)
+	  http.Error(w, err.Error(), http.StatusInternalServerError)
+	  return
 	}
-	ID, err := strconv.Atoi(val2["ID"])
-	if err != nil {
-		panic(err)
-	}
-
-	server := &Server{
-		Name:    val2["Name"],
-		ID:      ID,
-		Enabled: Enabled,
-	}
-
-	fmt.Println("struct", server)
-
-  w.Header().Set("Server", "A Go Web Server")
-
-	js, err := json.Marshal(server)
-	if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
 
 	w.Header().Set("Content-Type", "application/json")
-  w.Write(js)
+	w.Write(js)
 
 }
