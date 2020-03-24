d1ca655a945661fe6efe067cd44468788663a252
Erik Bergsma <ebergsma1@gmail.com>
2020-03-24 11:39:16 +0100

    Struct is now re-created in the listhandler function, after being fetched from Redis. this opens the door to printing it to Yaml or Json

--- src/golang/main.go
+++ src/golang/main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"github.com/fatih/structs"
 	"github.com/go-redis/redis"
+	//"reflect"
+	//"github.com/mitchellh/mapstructure"
+	"strconv"
 )
 
 var (
@@ -17,26 +20,14 @@ var (
 	authenticated = false
 )
 
+type Server struct {
+	Name        string
+	ID          int
+	Enabled     bool
+	users       []string // not exported
+}
 
 func main() {
-	err := client.Set("key", "value", 0).Err()
-	if err != nil {
-		panic(err)
-	}
-
-	val, err := client.Get("key").Result()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("key", val)
-
-	type Server struct {
-		Name        string
-		ID          int
-		Enabled     bool
-		users       []string // not exported
-	}
-
 	server := &Server{
 		Name:    "gopher",
 		ID:      123456,
@@ -48,30 +39,25 @@ func main() {
 	m := structs.Map(server)
 	fmt.Println("map", m)
 
-	client.HSet("customer:2", m)
-
-	val3 := client.HGetAll("customer:2" )
-	fmt.Println("key3", val3)
-
-	val4 := client.HGet("customer:2", "Name")
-	fmt.Println("key4", val4)
+	err := client.HSet("customer:2", m).Err()
+	if err != nil {
+		panic(err)
+	}
 
 	// route
-	//http.HandleFunc("/list", listHandler)
+	http.HandleFunc("/list", listHandler)
 	//http.HandleFunc("/create", createHandler)
 	//http.HandleFunc("/update", updateHandler)
 	//http.HandleFunc("/delete", deleteHandler)
-	//http.ListenAndServe(":3333", nil)
-
+	http.ListenAndServe(":3333", nil)
 }
-
 
 func listHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		http.Error(w, "Method not allowed", http.StatusBadRequest)
 	}
 
-	val2, err := client.Get("key2").Result()
+	val2, err := client.HGetAll("customer:2" ).Result()
 	if err == redis.Nil {
 		fmt.Println("key2 does not exist")
 	} else if err != nil {
@@ -80,4 +66,30 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("key2", val2)
 	}
 
+	Enabled, err := strconv.ParseBool(val2["Enabled"])
+	ID, err := strconv.Atoi(val2["ID"])
+
+	server := &Server{
+		Name:    val2["Name"],
+		ID:      ID,
+		Enabled: Enabled,
+	}
+
+	fmt.Println("struct", server)
+
+  //map[string]string
+  //fmt.Println(reflect.TypeOf(val2))
+	//var result Server
+	//err2 := mapstructure.Decode(val2, &result)
+
+	//fmt.Println(reflect.TypeOf(result))
+	//fmt.Println(reflect.TypeOf(err2))
+
+	//* 'Enabled' expected type 'bool', got unconvertible type 'string'
+	//* 'ID' expected type 'int', got unconvertible type 'string'
+	//fmt.Println("err", err2)
+	//if err2 != nil {
+  //  panic(err)
+	//}
+	//fmt.Println("key3", result)
 }
