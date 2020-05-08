71d5ab225aaaa03065c729d85521f1e7f10b3114
Erik Bergsma <ebergsma1@gmail.com>
2020-05-08 09:25:03 +0200

    dont need the foreach loop, split into a seperate js file

---
+++ src/web/frontend/webroot/script.js
@@ -0,0 +1,38 @@
+var app = angular.module('myApp', []);
+app.controller('customersCtrl', function($scope, $http) {
+  //get data from API
+  $http.get("/api/list")
+  .then(function (response) {
+    $scope.names = response.data;
+
+    angular.forEach($scope.names, function (item) {
+      if (item.Enabled){
+        item.selectedOption = {id: '1', name: 'true'}
+      } else{
+        item.selectedOption = {id: '2', name: 'false'}
+      }
+    })
+  });
+
+  //onclick handler
+  $scope.remove = function(array, index){
+    array.splice(index, 1);
+  }
+
+  $scope.update = function(array, index, id){
+    var name = $('#' + id + '_name').val()
+    var enabled = $('#' + id + '_enabled').val()
+
+    // i can just pass the index and the array, so i dont need to do a for loop ^^
+    array[index].Name = name;
+    array[index].Enabled = enabled;
+  }
+
+  $scope.data = {
+   availableOptions: [
+     {id: '1', name: 'true'},
+     {id: '2', name: 'false'}
+   ]
+  };
+
+});
